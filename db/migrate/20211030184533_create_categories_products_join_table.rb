d0d1f4c680d418b2d434247797b69982991f053c
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-10-30 16:08:57 -0300

    added relationship between products and categories tables

--- db/migrate/20211030184533_create_categories_products_join_table.rb
+++ db/migrate/20211030184533_create_categories_products_join_table.rb
@@ -1,4 +1,6 @@
 class CreateCategoriesProductsJoinTable < ActiveRecord::Migration[5.2]
   def change
+    #this migration makes posible to use "has_and_belongs_to_many" method on product's and category's models
+    create_join_table :categories, :products
   end
 end
