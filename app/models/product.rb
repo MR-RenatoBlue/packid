e314987142918dd0cd93502feb40595e48a0371d
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-11-08 14:22:37 -0300

    validation on product's price

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations
   
-  validates :name, :description, :price, :categories, presence: true
+  validates :name, :description, :categories, presence: true
+  validates :price, presence: true, numericality: true
 end
