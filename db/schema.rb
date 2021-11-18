d13d11ec0f2400d334d30439d88b9a4a78716dbb
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-11-18 13:53:32 -0300

    added user name to user's model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_140418) do
+ActiveRecord::Schema.define(version: 2021_11_18_165204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,9 @@ ActiveRecord::Schema.define(version: 2021_11_17_140418) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_seem_at"
     t.datetime "last_seen_at"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
