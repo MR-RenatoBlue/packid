b3b7aa1ad2f9bff00826d6a8cd1b127e767ba44a
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-11-19 14:22:11 -0300

    still wall feature with posts now

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   resources :users
-  resource :notifications, only: [:index]
+  resources :notifications
   root to: "home#index"
 end
